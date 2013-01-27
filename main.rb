c087cc2a0835f4fc9d9512932ddfbd29abb570cb
Michael Allan <michaelallan@optushome.com.au>
2013-01-27 15:44:06 +1100

    Moved templates from inline to separate files

--- main.rb
+++ main.rb
@@ -4,25 +4,3 @@ require 'slim'
 get '/' do
   slim :index
 end
-
-__END__
-
-@@layout
-doctype html
-html
-  head
-    meta charset="utf-8"
-    title Just Do It - Sample Sinatra application
-    link rel="stylesheet" media="screen, projection" href="/styles.css"
-    /[if lt IE 9]
-      script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"
-  body
-    h1 Just Do It, Mike!
-
-    == yield
-
-@@index
-h2 My Tasks
-ul.tasks
-  li Get Milk
-  li Build Sinatra To-Do list

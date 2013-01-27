0b4732fe2d326c8af379e7a42f01e2a48f416645
Michael Allan <michaelallan@optushome.com.au>
2013-01-27 18:20:30 +1100

    Add DataMapper with Task class and sample data

--- Gemfile
+++ Gemfile
@@ -2,3 +2,7 @@ source 'http://rubygems.org'
 
 gem 'sinatra'
 gem 'slim'
+
+gem 'data_mapper'
+gem 'dm-sqlite-adapter'
+gem 'sqlite3'
--- main.rb
+++ main.rb
@@ -1,5 +1,19 @@
 require 'sinatra'
 require 'slim'
+require 'data_mapper'
+
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite3://#{Dir.pwd}/development.db")
+
+class Task
+  include DataMapper::Resource
+
+  property :id,             Serial
+  property :name,           String, :required => true
+  property :completed_at,   DateTime
+end
+
+DataMapper.finalize
+
 
 get '/' do
   slim :index

23e0e3eebe509009822e59ac358b4ac0be42338c
Michael Allan <michaelallan@optushome.com.au>
2013-01-28 07:20:32 +1100

    Add ability to mark tasks as completed

--- main.rb
+++ main.rb
@@ -26,6 +26,13 @@ post '/' do
   redirect to('/')
 end
 
+put '/task/:id' do
+  task = Task.get params[:id]
+  task.completed_at = task.completed_at.nil? ? Time.now : nil
+  task.save
+  redirect to('/')
+end
+
 delete '/task/:id' do
   Task.get(params[:id]).destroy
   redirect to('/')
